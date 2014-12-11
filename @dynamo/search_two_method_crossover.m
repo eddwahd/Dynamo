519ed57b1f22a66aa4e2efb61d6f0470b0c22af6
Ville Bergholm <ville.bergholm@iki.fi>
2014-12-11 20:38:16 +0100

    Cleanup: Removed nonfunctional/unnecessary code incl. Krotov search.

--- @dynamo/search_two_method_crossover.m
+++
@@ -1,5 +0,0 @@
-function term_reason = Two_method_crossover(control_mask, first_method_term_cond, first_search_method, second_method_term_cond, second_search_method)
-
-first_search_method(control_mask, first_method_term_cond);
-
-term_reason = second_search_method(control_mask, second_method_term_cond);
